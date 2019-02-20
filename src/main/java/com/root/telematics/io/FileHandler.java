e7d93ec52a11499c1656ab6604b8a9381b62638b
Thomas Congrove <thomas.congrove@nationwide.com>
2019-02-20 14:47:00 -0500

    Ignoring blank file lines
    - Using StringBuffer

--- src/main/java/com/root/telematics/io/FileHandler.java
+++ src/main/java/com/root/telematics/io/FileHandler.java
@@ -8,16 +8,30 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FileHandler {
+	/**
+	 * This class will return a list of file lines, each of which is a list of
+	 * Strings. It will only add non-empty lines to the list
+	 * 
+	 * @param filePath
+	 * @return A list of String lists, each representing a line of the input file
+	 * @throws IOException
+	 */
 	public static List<List<String>> readTelemetryFile(String filePath) throws IOException {
 		FileInputStream inputStream = null;
 		Scanner sc = null;
 		List<List<String>> fileLines = new ArrayList<List<String>>();
 		inputStream = new FileInputStream(filePath);
 		sc = new Scanner(inputStream, "UTF-8");
+		StringBuffer line = new StringBuffer();
 
 		// Read each line of the space-delimited file into a list
 		while (sc.hasNextLine()) {
-			fileLines.add(Arrays.asList(sc.nextLine().split(" ")));
+			// StringBuffer is a little messy to read, but big files will appreciate it
+			if (!((line.append(sc.nextLine())).length() == 0)) {
+				fileLines.add(Arrays.asList(line.toString().split(" ")));
+				// Cleanup for memory management
+				line.setLength(0);
+			}
 		}
 
 		// Cleanup
